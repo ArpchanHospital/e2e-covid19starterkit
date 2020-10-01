8237be0fa12ff366182d288f0464f359547da257
Nikhitha Muskula <nikhitha.muskula@thoughtworks.com>
2020-10-01 14:21:11 +0530

    [Nikhitha/Harshita] add enter patient details

--- src/test/java/framework/PageFactory.java
+++ src/test/java/framework/PageFactory.java
@@ -6,6 +6,7 @@ public class PageFactory extends Wait {
     private static LoginPage loginPage;
     private static DashboardPage dashboardPage;
     private static RegistrationPage registrationPage;
+    private static PatientDetailsPage patientDetailsPage;
 
     public static LoginPage getLoginPage() {
         return loginPage;
@@ -19,9 +20,14 @@ public class PageFactory extends Wait {
         return registrationPage;
     }
 
+    public static PatientDetailsPage getPatientDetailsPage() {
+        return patientDetailsPage;
+    }
+
     public static void instantiatePages() {
         loginPage = new LoginPage();
         dashboardPage = new DashboardPage();
         registrationPage = new RegistrationPage();
+        patientDetailsPage = new PatientDetailsPage();
     }
 }
--- src/test/java/tests/CovidUserJourneyTest.java
+++ src/test/java/tests/CovidUserJourneyTest.java
@@ -5,7 +5,14 @@ import framework.LoggerClass;
 import org.apache.log4j.Level;
 import org.testng.annotations.Test;
 
+import java.util.Random;
+
 public class CovidUserJourneyTest extends BaseTestClass {
+    Random random = new Random();
+    char nextRandomChar = (char) (65 + (int) (random.nextFloat() * (90 - 65 + 1)));
+    public String firstName = "covid" + nextRandomChar;
+    public String lastName = "patient" + nextRandomChar;
+
     @Test
     public void testCovidUserJourney() {
         getLoginPage().login();
@@ -16,5 +23,8 @@ public class CovidUserJourneyTest extends BaseTestClass {
 
         getRegistrationPage().clickCreateNew();
         LoggerClass.showLog(CovidUserJourneyTest.class, Level.INFO, "clicked on createNew");
+
+        getPatientDetailsPage().fillPatientDetails(firstName, lastName);
+        LoggerClass.showLog(CovidUserJourneyTest.class, Level.INFO, "filled patient details and started emergency visit");
     }
 }
